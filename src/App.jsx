4ab5f42c2c7095f8240ff06e839d2c8cde4b2880
Arshil <arshilgondaliya5@gmail.com>
2025-07-18 13:18:57 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { BiDetail } from 'react-icons/bi';
+import { FaFacebook, FaGithub, FaLinkedinIn, FaRegEnvelope, FaTwitter, FaUser } from 'react-icons/fa';
+import { IoBriefcaseOutline, IoHome } from "react-icons/io5";
+
+const App = () => {
+  return (
+    <div>
+      {/* navbar  */}
+
+      <nav className="nav fixed bottom-[2rem] left-[50%] w-[40rem] rounded-[5rem] z-[100] ">
+        <ul className="nav-list flex justify-evenly">
+          <li className='relative flex text-[3rem] cursor-pointer active'>
+            <IoHome />
+            <span className='tooltip absolute top-[-4rem] left-[50%] rounded-[.4rem] text-[1.6rem]'>Home</span>
+          </li>
+          <li className='relative flex text-[3rem] cursor-pointer'>
+            <FaUser />
+            <span className='tooltip  absolute top-[-4rem] left-[50%] rounded-[.4rem] text-[1.6rem]'>About</span>
+          </li>
+          <li className='relative flex text-[3rem] cursor-pointer'>
+            <BiDetail />
+            <span className='tooltip  absolute top-[-4rem] left-[50%] rounded-[.4rem] text-[1.6rem]'>Resume</span>
+          </li>
+          <li className='relative flex text-[3rem] cursor-pointer'>
+            <IoBriefcaseOutline />
+            <span className='tooltip  absolute top-[-4rem] left-[50%] rounded-[.4rem] text-[1.6rem]'>Portfolio</span>
+          </li>
+          <li className='relative flex text-[3rem] cursor-pointer'>
+            <FaRegEnvelope />
+            <span className='tooltip  absolute top-[-4rem] left-[50%] rounded-[.4rem] text-[1.6rem]'>Contact</span>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container w-[100vw] h-[100vh]">
+        <div className="box relative w-[100%] h-[100%]">
+          {/* Home section  */}
+          <section className="section home ">
+            <div className="home-info">
+              <h3>Hello I'm</h3>
+              <h1>Arshil</h1>
+              <h3>Full Stack Developer</h3>
+              <p className='desc'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet tempore minus pariatur blanditiis deleniti voluptate magnam, maxime neque totam ipsum. 
+              </p>
+              <div className="btn-sci  ">
+                <a href="#" className='btn  '>Download CV </a>
+                <div className="sci ">
+                  <a href="#">
+                    <FaGithub className='icon' />
+                    {/* <span className='tooltip'>Github</span> */}
+                  </a>
+                  <a href="#">
+                    <FaLinkedinIn className='icon' />
+                    {/* <span className='tooltip'>Linkedin</span> */}
+                  </a>
+                  <a href="#">
+                    <FaTwitter className='icon' />
+                    {/* <span className='tooltip'>Twitter</span> */}
+                  </a>
+                  <a href="#">
+                    <FaFacebook className='icon' />
+                    {/* <span className='tooltip '>Facebook</span> */}
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="img-wrapper">
+              <div className="img-box home-img">
+                <img src="image\my image.png" alt="" />
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BiDetail } from 'react-icons/bi';
+import { FaFacebook, FaGithub, FaLinkedinIn, FaRegEnvelope, FaTwitter, FaUser } from 'react-icons/fa';
+import { IoBriefcaseOutline, IoHome } from "react-icons/io5";
+
+const Navbar = () => {
+    return (
+        <div>
+         
+
+        </div>
+    )
+}
+
+export default Navbar
